9d68389697f7a08eaddbc9bb1eed5b48c9c8c7ff
Rhys <lomirus@qq.com>
2021-03-03 15:22:37 +0800

    refactor: develop fundamental structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,115 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-class MainPrcs extends React.Component {
+class App extends React.Component {
     constructor (props) {
         super(props)
     }
     render () {
-        return <div>Hello World</div>
+        return <div>
+            <BoxWrapper />
+            <Tabs />
+        </div>
     }
 }
 
-ReactDom.render(<p>Hello World</p>, document.querySelector('#root'))
+class Tabs extends React.Component {
+    constructor (props) {
+        super(props)
+    }
+    render () {
+        return <div id="tabs">
+            <Tab type="test" />
+            <Tab type="list" />
+            <Tab type="conf" />
+        </div>
+    }
+}
+
+class Tab extends React.Component {
+    constructor (props) {
+        super(props)
+    }
+    render () {
+        return <div></div>
+    }
+}
+
+class BoxWrapper extends React.Component {
+    constructor (props) {
+        super(props)
+    }
+    render () {
+        return <div>
+            <TestBox />
+            <ListBox />
+            <ConfBox />
+        </div>
+    }
+}
+
+class Box extends React.Component {
+    constructor (props) {
+        super(props)
+    }
+}
+
+class PrepBox extends Box {
+    constructor (props) {
+        super(props)
+    }
+    render () {
+        return <div></div>
+    }
+}
+
+class PrcsBox extends Box {
+    constructor (props) {
+        super(props)
+    }
+    render () {
+        return <div></div>
+    }
+}
+
+class FnshBox extends Box {
+    constructor (props) {
+        super(props)
+    }
+    render () {
+        return <div></div>
+    }
+}
+
+class TestBox extends Box {
+    constructor (props) {
+        super(props)
+    }
+    render () {
+        return <div>
+            <PrepBox />
+            <PrcsBox />
+            <FnshBox />
+        </div>
+    }
+}
+
+class ConfBox extends Box {
+    constructor (props) {
+        super(props)
+    }
+    render () {
+        return <div></div>
+    }
+}
+
+class ListBox extends Box {
+    constructor (props) {
+        super(props)
+    }
+    render () {
+        return <div></div>
+    }
+}
+
+ReactDom.render(<App />, document.querySelector('#root'))
